8e0c7c29d93d056b4fa5b4638ccd663e2fab29d9
Marcus <54276056+individualtool@users.noreply.github.com>
2019-09-12 09:16:14 -0500

    Add files via upload

---
+++ vehicle/Model.java
@@ -0,0 +1,30 @@
+package vehicle;
+
+public class Model {
+	private String BodyType;
+	private String ModelName;
+	private String[] TrimPackage;
+	
+	public String getBodyType() {
+		return BodyType;
+	}
+	public void setBodyType(String bodyType) {
+		BodyType = bodyType;
+	}
+	public String getModelName() {
+		return ModelName;
+	}
+	public void setModelName(String modelName) {
+		ModelName = modelName;
+	}
+	public String[] getTrimPackage() {
+		return TrimPackage;
+	}
+	public void setTrimPackage(String[] trimPackage) {
+		TrimPackage = trimPackage;
+	}
+	
+	
+}
+	
+
